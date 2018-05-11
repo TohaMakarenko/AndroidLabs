b801819484920c9a634e800884bce801ef3f180c
Anton Makarenko <demo6112@gmail.com>
2018-05-11 22:17:59 +0300

    lab2

---
+++ Lab2/app/src/main/java/com/makarenko/lab2/PasswordFragment.java
@@ -0,0 +1,66 @@
+package com.makarenko.lab2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class PasswordFragment extends Fragment {
+    EditText passEdit;
+    Switch switchPasss;
+    Button okButton;
+    int defInputType;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_password, container, false);
+
+        passEdit = view.findViewById(R.id.passwordEdit);
+        switchPasss = view.findViewById(R.id.switchPass);
+        okButton = view.findViewById(R.id.OkButton);
+        defInputType = passEdit.getInputType();
+        final Context context = view.getContext();
+
+        okButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Password");
+                builder.setMessage(passEdit.getText());
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        switchPasss.setOnCheckedChangeListener( new Switch.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    passEdit.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+                else {
+                    passEdit.setInputType(defInputType);
+                }
+            }
+        });
+
+        return view;
+    }
+}
